ccccb00f152681fe4b9e56a49e400cb55f39a1e1
Douglas Cruz <douglas14dgs@hotmail.com>
2020-10-29 22:23:15 -0300

    Primeiro commit

---
+++ src/server.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const server = express()
+
+const { pageLanding, pageStudy, pageGiveClasses, saveClasses, pageSuccess } = require('./pages')
+
+const nunjucks = require('nunjucks')
+
+nunjucks.configure('src/views', {
+    express: server,
+    noCache: true
+})
+
+server
+.use(express.urlencoded({ extended: true }))
+.use(express.static("public"))
+.get("/", pageLanding)
+.get("/study", pageStudy)
+.get("/give-classes", pageGiveClasses)
+.get("/success", pageSuccess)
+.post("/save-classes", saveClasses)
+.listen(5500)
